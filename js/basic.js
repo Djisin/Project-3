24f88031a5ab735e4b15fcf2057ff0023586f124
Djisin <medija_teka@hotmail.com>
2019-11-27 04:12:25 +0100

    project 3 1st upload

---
+++ js/basic.js
@@ -0,0 +1,17 @@
+var myVar;
+function myFunction() {
+    myVar = setTimeout(showPage, 1000);
+}          
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    if(window.location.pathname == '/index.html') {
+        document.getElementById("home-page").style.display = "block";
+    }
+    else if (window.matchMedia('(max-width: 720px)').matches) {
+        document.getElementById("container").style.display = "flex";
+    }
+    else{
+        
+        document.getElementById("container").style.display = "grid";
+    }
+}

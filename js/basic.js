250f9c325d4f39e9810193cc01477addc0bce6d6
Djisin <medija_teka@hotmail.com>
2019-11-27 04:45:14 +0100

    path fix #5

--- js/basic.js
+++ js/basic.js
@@ -4,10 +4,10 @@ function myFunction() {
 }          
 function showPage() {
     document.getElementById("loader").style.display = "none";
-    if(window.location.pathname == 'index.html') {
+    if(window.location.pathname == "./index.html") {
         document.getElementById("home-page").style.display = "block";
     }
-    else if (window.matchMedia('(max-width: 720px)').matches) {
+    else if (window.matchMedia("(max-width: 720px)").matches) {
         document.getElementById("container").style.display = "flex";
     }
     else{
